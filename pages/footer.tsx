bd0871532a0ce1670b2d43dd9d52af1cf151604c
luke <lukegough@me.com>
2024-03-13 16:44:13 -0400

    yes

---
+++ pages/footer.tsx
@@ -0,0 +1,10 @@
+import "@/styles/footer.css"
+
+export default function Footer() {
+    return (<div className="footer">
+        <a className="footerlink">GitHub</a>
+        <a className="footerlink">LinkedIn</a>
+        <a className="footerlink">Vimeo</a>
+        <a className="footerlink">Email</a>
+    </div>);
+}
